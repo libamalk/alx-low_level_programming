c8d0d0c6254c71937a2f2a31c9958dab3ef78774
libamalk <assefakahsu1234@gmail.com>
2022-06-13 07:38:36 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - check the code
+ * @str: pointer int type
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
